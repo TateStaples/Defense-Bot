f7bfa8e116b3478487e0e686af48dfe128bf553e
TateStaples <56731352+tatestaples@users.noreply.github.com>
2020-03-07 10:29:04 -0500

    improved joystick

--- src/main/kotlin/frc/team6502/robot/Constants.kt
+++ src/main/kotlin/frc/team6502/robot/Constants.kt
@@ -1,5 +1,7 @@
 package frc.team6502.robot
 
+import kotlin.math.PI
+
 /**
  * Define any constants in this file.
  */
@@ -10,4 +12,13 @@ object Constants {
     const val BACK_RIGHT_PORT = 3
 
     const val JOYSTICK_PORT = 0
+
+    const val FORWARD_DEADBAND = 0.2
+    const val FORWARD_RATE = -12.0
+    const val FORWARD_EXPO = 20.0
+
+    const val TURN_DEADBAND = 0.1
+    const val TURN_RATE =  -5 * PI
+    const val TURN_EXPO = 73.0
+
 }
